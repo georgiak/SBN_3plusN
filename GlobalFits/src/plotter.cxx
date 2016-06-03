04bc81362d833b6e6b8895c3bf39d17895a6fb45
Davio Cianci <dcianci@ppdhcp87.hep.manchester.ac.uk>
2016-06-03 12:22:58 +0100

    Prettied up plots

--- GlobalFits/src/plotter.cxx
+++ GlobalFits/src/plotter.cxx
@@ -1,10 +1,12 @@
 #include "TLegend.h"
+#include "TH3F.h"
 #include "globalFit.h"
 #include "TCut.h"
+#include "TView3D.h"
 bool procOpt();
 
 std::string plotOutput = "plots";
-int steriles, nRuns, type, raster, discretized, diag;
+int steriles, nRuns, type, raster, discretized, diag, dims;
 std::string dataset, location, output;
 std::string procOptLoc;
 std::string suffix;
@@ -41,164 +43,248 @@ int globFit_plotter(){
 
 	TCanvas *c1 = new TCanvas("c1");
 
-	// Setup histo
-    TH2F *h = new TH2F("h","3+3 #Chi^2;U_{e 4};U_{#mu 4}",1000,0.01,100.,1000,.01,100.);
-    h->GetXaxis()->SetTitleOffset(1.1);
-    h->GetYaxis()->SetTitleOffset(.8);
-    h->GetXaxis()->SetTitleFont(62);
-    h->GetYaxis()->SetTitleFont(62);
-    h->GetYaxis()->CenterTitle();
-    h->GetXaxis()->CenterTitle();
-    h->GetXaxis()->SetTitleSize(0.04);
-    h->GetXaxis()->SetLabelSize(0.04);
-    h->GetXaxis()->SetLabelOffset(0.001);
-    h->GetYaxis()->SetTitleSize(0.05);
-    h->GetYaxis()->SetLabelSize(0.04);
-    h->SetStats(kFALSE);
+	if(dims == 2){
+		// Setup histo
+    	TH2F *h = new TH2F("h","3+3 #Chi^2;U_{e 4};U_{#mu 4}",1000,0.01,100.,1000,.01,100.);
+    	h->GetXaxis()->SetTitleOffset(1.1);
+    	h->GetYaxis()->SetTitleOffset(.8);
+    	h->GetXaxis()->SetTitleFont(62);
+    	h->GetYaxis()->SetTitleFont(62);
+    	h->GetYaxis()->CenterTitle();
+    	h->GetXaxis()->CenterTitle();
+    	h->GetXaxis()->SetTitleSize(0.04);
+    	h->GetXaxis()->SetLabelSize(0.04);
+    	h->GetXaxis()->SetLabelOffset(0.001);
+    	h->GetYaxis()->SetTitleSize(0.05);
+    	h->GetYaxis()->SetLabelSize(0.04);
+    	h->SetStats(kFALSE);
 
-	c1->SetLogy();
-	c1->SetLogx();
-	chi2_99->SetMarkerStyle(7);
-	chi2_90->SetMarkerStyle(7);
-	chi2_99->SetFillColor(kBlue);
-    chi2_90->SetFillColor(kMagenta);
-	chi2_99->SetMarkerColor(kBlue);
-	chi2_90->SetMarkerColor(kMagenta);
-	if(diag == 1){
-		chi2_95->SetMarkerStyle(7);
-		chi2_95->SetMarkerColor(kRed+3);
-	}
-
-	// Make overlay of paper plots for diag
-	TGraph *overlay; Double_t x95[2100]; Double_t y95[2100];
-	if(diag == 1){
-		std::cout << " Diagnostic plot" << std::endl;
-		std::string det;
-		if(dataset == "karmen") det = "KARMEN";
-		if(dataset == "ccfr") det = "CCFR";
-		if(dataset == "cdhs") det = "CDHS";
-		if(dataset == "lsnd") det = "LSND";
-		if(dataset == "nomad") det = "nomad";
-		if(dataset == "xsec") det = "XSEC";
-		if(dataset == "bugey") det = "BUGEY";
-		if(dataset == "numi") det = "NUMI";
-		if(dataset == "mbnu") det = "MBNU";
-		if(dataset == "mbnubar") det = "MBNUBAR";
-		if(dataset == "mbnudis") det = "MBNUDIS";
-		if(dataset == "gal") det = "GAL";
-		if(dataset == "minos") det = "MINOS";
-		std::string overFile = det + ".csv";
-		std::cout << "Overlay: " << overFile << std::endl;
-
-		if(overFile != ".csv"){
-			ifstream file;
-	    	file.open(overFile);
-	    	for(int i = 0; i < 2100; i++){
-	        	file >> x95[i];
-				file >> y95[i];
-			}
-	    	file.close();
+		c1->SetLogy();
+		c1->SetLogx();
+		chi2_99->SetMarkerStyle(7);
+		chi2_90->SetMarkerStyle(7);
+		chi2_99->SetFillColor(kBlue);
+    	chi2_90->SetFillColor(kMagenta);
+		chi2_99->SetMarkerColor(kBlue);
+		chi2_90->SetMarkerColor(kMagenta);
+		if(diag == 1){
+			chi2_95->SetMarkerStyle(7);
+			chi2_95->SetFillColor(kRed+3);
+			chi2_95->SetMarkerColor(kRed+3);
 		}
 
-		overlay = new TGraph(2100,x95,y95);
-		overlay->SetFillColor(kRed);
-		overlay->SetMarkerColor(kRed);
-		overlay->SetMarkerStyle(7);
-	}
+		// Make overlay of paper plots for diag
+		TGraph *overlay; Double_t x95[2100]; Double_t y95[2100];
+		if(diag == 1){
+			std::cout << " Diagnostic plot" << std::endl;
+			std::string det;
+			if(dataset == "karmen") det = "KARMEN";
+			if(dataset == "ccfr") det = "CCFR";
+			if(dataset == "cdhs") det = "CDHS";
+			if(dataset == "lsnd") det = "LSND";
+			if(dataset == "nomad") det = "nomad";
+			if(dataset == "xsec") det = "XSEC";
+			if(dataset == "bugey") det = "BUGEY";
+			if(dataset == "numi") det = "NUMI";
+			if(dataset == "mbnu") det = "MBNU";
+			if(dataset == "mbnubar") det = "MBNUBAR";
+			if(dataset == "mbnudis") det = "MBNUDIS";
+			if(dataset == "gal") det = "GAL";
+			if(dataset == "minos") det = "MINOS";
+			std::string overFile = det + ".csv";
+			std::cout << "Overlay: " << overFile << std::endl;
+
+			if(overFile != ".csv"){
+				ifstream file;
+	    		file.open(overFile);
+	    		for(int i = 0; i < 2100; i++){
+	        		file >> x95[i];
+					file >> y95[i];
+				}
+	    		file.close();
+			}
+
+			overlay = new TGraph(2100,x95,y95);
+			overlay->SetFillColor(kAzure+1);
+			overlay->SetMarkerColor(kAzure+1);
+			overlay->SetMarkerStyle(7);
+		}
 
 
-	TLegend *leg = new TLegend(0.7,0.7,0.95,0.9);
-	leg->SetFillStyle(0);
-	leg->SetFillColor(0);
-	leg->SetBorderSize(0);
-	leg->SetTextFont(62);
-	leg->SetTextSize(0.03);
-	leg->AddEntry(chi2_99,"99%% CL","f");
-	leg->AddEntry(chi2_90,"90%% CL","f");
+		TLegend *leg = new TLegend(0.1,0.1,0.35,0.3);
+		leg->SetFillStyle(0);
+		leg->SetFillColor(0);
+		leg->SetBorderSize(0);
+		leg->SetTextFont(62);
+		leg->SetTextSize(0.03);
+		if(diag == 0){
+			leg->AddEntry(chi2_99,"99%% CL","f");
+			leg->AddEntry(chi2_90,"90%% CL","f");
+		}
+		if(diag == 1){
+			leg->AddEntry(chi2_95,"Davio's fits","f");
+			leg->AddEntry(overlay,"Paper fits","f");
+		}
 
 
-	if(steriles == 3){
-		h->SetTitle("#chi^{2} for 3+3 Sterile Fits;#Delta m^{2}_{41};#Delta m^{2}_{51}");
-		h->GetXaxis()->SetLimits(.01,100.);
-		h->GetYaxis()->SetLimits(.01,100.);
-		h->Draw();
-
-		chi2_99->Draw("m5*m5:m4*m4","","same");
-		chi2_90->Draw("m5*m5:m4*m4","","same");
-		leg->Draw();
-        c1->Print((plotOutput + "/" + dataset + "_dm251xdm241" + suffix + ".png").c_str());
-
-		h->SetTitle("#chi^{2} for 3+3 Sterile Fits;#Delta m^{2}_{41};#Delta m^{2}_{61}");
-		h->GetXaxis()->SetLimits(.01,100.);
-		h->GetYaxis()->SetLimits(.01,100.);
-		h->Draw();
-
-		chi2_99->Draw("(m6*m6):(m4*m4)","","same");
-		chi2_90->Draw("(m6*m6):(m4*m4)","","same");
-		leg->Draw();
-        c1->Print(("plots/" + dataset + "_dm261xdm241" + suffix + ".png").c_str());
-	}
-
-	if(steriles == 2){
-		h->SetTitle("#chi^{2} for 3+2 Sterile Fits;#Delta m^{2}_{41};#Delta m^{2}_{51}");
-		h->GetXaxis()->SetLimits(.01,100.);
-		h->GetYaxis()->SetLimits(.01,100.);
-		h->Draw();
-
-		chi2_99->Draw("m5*m5:m4*m4","","same");
-		chi2_90->Draw("m5*m5:m4*m4","","same");
-		leg->Draw();
-        c1->Print((plotOutput + "/" + dataset + "_3plus2_dm251xdm241" + suffix + ".png").c_str());
-	}
-
-
-	if(steriles == 1){
-		if(raster == 0){
-			if(type==0)	h->SetTitle("#chi^{2} for 3+1 Sterile Fits;sin^{2}(2#Theta_{e#mu});#Delta m^{2}_{41}");
-			if(type==1)	h->SetTitle("#chi^{2} for 3+1 Sterile Fits;sin^{2}(2#Theta_{#mu#mu});#Delta m^{2}_{41}");
-			if(type==2)	h->SetTitle("#chi^{2} for 3+1 Sterile Fits;sin^{2}(2#Theta_{ee});#Delta m^{2}_{41}");
-			if(type==0)	h->GetXaxis()->SetLimits(.0001,.1);
-			if(type>0)	h->GetXaxis()->SetLimits(.0001,1.);
+		if(steriles == 3){
+			h->SetTitle("#chi^{2} for 3+3 Sterile Fits;#Delta m^{2}_{41};#Delta m^{2}_{51}");
+			h->GetXaxis()->SetLimits(.01,100.);
 			h->GetYaxis()->SetLimits(.01,100.);
 			h->Draw();
 
-			// Now, draw overlay of the old one
-			if(diag == 1){
-				overlay->Draw("psame");
+			chi2_99->Draw("m5*m5:m4*m4","","same");
+			chi2_90->Draw("m5*m5:m4*m4","","same");
+			leg->Draw();
+        	c1->Print((plotOutput + "/" + dataset + "_dm251xdm241" + suffix + ".png").c_str());
+
+			h->SetTitle("#chi^{2} for 3+3 Sterile Fits;#Delta m^{2}_{41};#Delta m^{2}_{61}");
+			h->GetXaxis()->SetLimits(.01,100.);
+			h->GetYaxis()->SetLimits(.01,100.);
+			h->Draw();
+
+			chi2_99->Draw("(m6*m6):(m4*m4)","","same");
+			chi2_90->Draw("(m6*m6):(m4*m4)","","same");
+			leg->Draw();
+        	c1->Print(("plots/" + dataset + "_dm261xdm241" + suffix + ".png").c_str());
+		}
+
+		if(steriles == 2){
+			h->SetTitle("#chi^{2} for 3+2 Sterile Fits;#Delta m^{2}_{41};#Delta m^{2}_{51}");
+			h->GetXaxis()->SetLimits(.01,100.);
+			h->GetYaxis()->SetLimits(.01,100.);
+			h->Draw();
+
+			chi2_99->Draw("m5*m5:m4*m4","","same");
+			chi2_90->Draw("m5*m5:m4*m4","","same");
+			leg->Draw();
+        	c1->Print((plotOutput + "/" + dataset + "_3plus2_dm251xdm241" + suffix + ".png").c_str());
+		}
+
+		if(steriles == 1){
+			if(raster == 0){
+				if(type==0)	h->SetTitle("#chi^{2} for 3+1 Sterile Fits;sin^{2}(2#Theta_{e#mu});#Delta m^{2}_{41}");
+				if(type==1)	h->SetTitle("#chi^{2} for 3+1 Sterile Fits;sin^{2}(2#Theta_{#mu#mu});#Delta m^{2}_{41}");
+				if(type==2)	h->SetTitle("#chi^{2} for 3+1 Sterile Fits;sin^{2}(2#Theta_{ee});#Delta m^{2}_{41}");
+				if(type==0)	h->GetXaxis()->SetLimits(.0001,.1);
+				if(type>0)	h->GetXaxis()->SetLimits(.0001,1.);
+				h->GetYaxis()->SetLimits(.01,100.);
+				h->Draw();
+
+				// Now, draw overlay of the old one
+				if(diag == 1){
+					chi2_95->Draw("dm2:sin22th","","same");
+					overlay->Draw("psame");
+					leg->Draw();
+				}
+				else{
+					if(type==0){
+        				chi2_99->Draw("m4**2:4*um4**2*ue4**2","","same");
+        				chi2_90->Draw("m4**2:4*um4**2*ue4**2","","same");
+					}
+					else if(type == 1){
+						chi2_99->Draw("m4*m4:4*um4*um4*(1-um4*um4)","","same");
+        				chi2_90->Draw("m4*m4:4*um4*um4*(1-um4*um4)","","same");
+					}
+					else if(type == 2){
+						chi2_99->Draw("m4*m4:4*ue4*ue4*(1-ue4*ue4)","","same colz");
+        				chi2_90->Draw("m4*m4:4*ue4*ue4*(1-ue4*ue4)","","same");
+					}
+					leg->Draw();
+				}
+        		c1->Print((plotOutput + "/" + dataset + "_3plus1_dm241xsinsq2t" + suffix + ".png").c_str());
+			}
+			if(raster == 1){
+				if(type==0)	h->SetTitle("95%%CL for 3+1 Sterile Fits;sin^{2}(2#Theta_{e#mu});#Delta m^{2}_{41}");
+				if(type==1)	h->SetTitle("95%%CL for 3+1 Sterile Fits;sin^{2}(2#Theta_{#mu#mu});#Delta m^{2}_{41}");
+				if(type==2)	h->SetTitle("95%%CL for 3+1 Sterile Fits;sin^{2}(2#Theta_{ee});#Delta m^{2}_{41}");
+				if(type==0)	h->GetXaxis()->SetLimits(.0001,.1);
+				if(type>0)	h->GetXaxis()->SetLimits(.0001,1.);
+				h->GetYaxis()->SetLimits(.01,100.);
+				h->Draw();
+
+				if(diag == 1)
+					overlay->Draw("psame");
 				chi2_95->Draw("dm2:sin22th","","same");
-			}
-			else{
-				if(type==0){
-        			chi2_99->Draw("m4**2:4*um4**2*ue4**2","","same");
-        			chi2_90->Draw("m4**2:4*um4**2*ue4**2","","same");
-				}
-				else if(type == 1){
-					chi2_99->Draw("m4*m4:4*um4*um4*(1-um4*um4)","","same");
-        			chi2_90->Draw("m4*m4:4*um4*um4*(1-um4*um4)","","same");
-				}
-				else if(type == 2){
-					chi2_99->Draw("m4*m4:4*ue4*ue4*(1-ue4*ue4):chi2","","same colz");
-        			//chi2_90->Draw("m4*m4:4*ue4*ue4*(1-ue4*ue4)","","same");
-				}
 				leg->Draw();
+				c1->Print((plotOutput + "/" + dataset + "_3plus1_dm241xsinsq2t_raster" + suffix + ".png").c_str());
 			}
-        	c1->Print((plotOutput + "/" + dataset + "_3plus1_dm241xsinsq2t" + suffix + ".png").c_str());
 		}
-		if(raster == 1){
+	}
 
-			if(type==0)	h->SetTitle("95%%CL for 3+1 Sterile Fits;sin^{2}(2#Theta_{e#mu});#Delta m^{2}_{41}");
-			if(type==1)	h->SetTitle("95%%CL for 3+1 Sterile Fits;sin^{2}(2#Theta_{#mu#mu});#Delta m^{2}_{41}");
-			if(type==2)	h->SetTitle("95%%CL for 3+1 Sterile Fits;sin^{2}(2#Theta_{ee});#Delta m^{2}_{41}");
-			if(type==0)	h->GetXaxis()->SetLimits(.0001,.1);
-			if(type>0)	h->GetXaxis()->SetLimits(.0001,1.);
-			h->GetYaxis()->SetLimits(.01,100.);
-			h->Draw();
+	if(dims == 3 && steriles == 1){
 
-			if(diag == 1)
-				overlay->Draw("psame");
-			chi2_95->Draw("dm2:sin22th","","same");
-			c1->Print((plotOutput + "/" + dataset + "_3plus1_dm241xsinsq2t_raster" + suffix + ".png").c_str());
+		// Shit, so the plots we want are dm2 vs sin22thmm, vs sin22thme and dm2 vs ue4, vs um4
+		TH3F *h = new TH3F("h1","3+1 #Chi^{2};U_{#mu 4};U_{e 4};#Delta m^{2}_{41}",1000,0.0001,1.,1000,0.0001,1.,1000,0.01,100.);
+		TH3F *h3d1_99 = new TH3F("h2","",1000,0.0001,1.,1000,0.0001,1.,1000,0.01,100.);
+		TH3F *h3d1_90 = new TH3F("h3","",1000,0.0001,1.,1000,0.0001,1.,1000,0.01,100.);
+		TH3F *h3d2_99 = new TH3F("h4 ","",1000,0.,.5,1000,0.,.5,1000,0.01,100.);
+		TH3F *h3d2_90 = new TH3F("h5","",1000,0.,.5,1000,0.,.5,1000,0.01,100.);
+
+		h->GetXaxis()->SetTitleOffset(1.4);
+		h->GetYaxis()->SetTitleOffset(1.6);
+		h->GetZaxis()->SetTitleOffset(.9);
+		h->GetXaxis()->SetTitleFont(62);
+		h->GetYaxis()->SetTitleFont(62);
+		h->GetZaxis()->SetTitleFont(62);
+		h->GetYaxis()->CenterTitle();
+		h->GetXaxis()->CenterTitle();
+		h->GetZaxis()->CenterTitle();
+		h->GetXaxis()->SetTitleSize(0.04);
+		h->GetXaxis()->SetLabelSize(0.035);
+		h->GetYaxis()->SetTitleSize(0.04);
+		h->GetYaxis()->SetLabelSize(0.035);
+		h->GetYaxis()->SetTitleSize(0.04);
+		h->GetYaxis()->SetLabelSize(0.035);
+    	h->SetStats(kFALSE);
+
+		c1->SetLogz();
+		h3d1_99->SetMarkerStyle(7);			h3d2_99->SetMarkerStyle(7);
+		h3d1_90->SetMarkerStyle(7);			h3d2_90->SetMarkerStyle(7);
+		h3d1_99->SetFillColor(kBlue);		h3d2_99->SetFillColor(kBlue);
+    	h3d1_90->SetFillColor(kMagenta);	h3d2_90->SetFillColor(kMagenta);
+		h3d1_99->SetMarkerColor(kBlue);		h3d2_99->SetMarkerColor(kBlue);
+		h3d1_90->SetMarkerColor(kMagenta);	h3d2_90->SetMarkerColor(kMagenta);
+
+		// Now fill the histos:
+		// First, 99%
+		float m4, ue4, um4;
+		chi2_99->SetBranchAddress("m4",&m4);
+		chi2_99->SetBranchAddress("ue4",&ue4);
+		chi2_99->SetBranchAddress("um4",&um4);
+		float mstep = TMath::Log10(100./.01)/float(1000);
+		float ustep = (.5)/float(1000);
+		float sinstep = TMath::Log10(1./.0001)/float(1000);
+		float sinsmm, sinsme;
+		for(int i = 0; i < chi2_99->GetEntries(); i++){
+			chi2_99->GetEntry(i);
+			sinsme = 4*pow(um4,2)*pow(ue4,2); 	sinsmm = 4*pow(um4,2)*(1-pow(um4,2));
+			h3d1_99->SetBinContent(ceil(TMath::Log10(sinsme/.0001)/sinstep),ceil(TMath::Log10(sinsmm/.0001)/sinstep),ceil(TMath::Log10(pow(m4,2)/.01)/mstep),1);
+			h3d2_99->SetBinContent(ceil(um4/ustep),ceil(ue4/ustep),ceil(TMath::Log10(pow(m4,2)/.01)/mstep),1);
 		}
+		// now 90
+		chi2_90->SetBranchAddress("m4",&m4);
+		chi2_90->SetBranchAddress("ue4",&ue4);
+		chi2_90->SetBranchAddress("um4",&um4);
+		for(int i = 0; i < chi2_90->GetEntries(); i++){
+			chi2_90->GetEntry(i);
+			sinsme = 4*pow(um4,2)*pow(ue4,2); 	sinsmm = 4*pow(um4,2)*(1-pow(um4,2));
+			h3d1_90->SetBinContent(ceil(TMath::Log10(sinsme/.0001)/sinstep),ceil(TMath::Log10(sinsmm/.0001)/sinstep),ceil(TMath::Log10(pow(m4,2)/.01)/mstep),1);
+			h3d2_90->SetBinContent(ceil(um4/ustep),ceil(ue4/ustep),ceil(TMath::Log10(pow(m4,2)/.01)/mstep),1);
+		}
+
+		//h->Draw();
+		h3d2_99->Draw("p");
+		h3d2_90->Draw("psame");
+		c1->Print((plotOutput + "/" + dataset + "_threedee_ues" + suffix + ".png").c_str());
+
+		h->SetTitle("3+1 #Chi^{2};sin^{2}(2#Theta_{e#mu});sin^{2}(2#Theta_{#mu#mu});#Delta m^{2}_{41}");
+		c1->SetLogx();
+		c1->SetLogy();
+		h->Draw();
+		h3d1_99->Draw("psame");
+		h3d1_90->Draw("psame");
+		c1->Print((plotOutput + "/" + dataset + "_threedee_sinsqs" + suffix + ".png").c_str());
 	}
 
     return 0;
@@ -265,6 +351,12 @@ bool procOpt(){
 	std::getline(is_line9,key,'=');
 	std::getline(is_line9,value);
  	diag = atoi(value.c_str());
+
+	std::getline(file,line);
+	std::istringstream is_line10(line);
+	std::getline(is_line10,key,'=');
+	std::getline(is_line10,value);
+ 	dims = atoi(value.c_str());
 
     return true;
 }
