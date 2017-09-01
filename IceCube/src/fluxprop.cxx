91c6e6fc20b5f96a717ae61cdf99b492a196f151
dcianci <dccianci@gmail.com>
2017-09-01 17:35:03 -0400

    Flux propagation extravaganza!

--- IceCube/src/fluxprop.cxx
+++ IceCube/src/fluxprop.cxx
@@ -1,3 +1,16 @@
+// fluxprop.cxx by Davio Cianci
+// 9/1/2017
+//
+// This file takes the initial unpropagated flux of either pions or kaons (by using -p or -k in execution) and propagates them through the earth
+// Then it takes the propagated flux at icecube and bin averages according to icecube's true neutrino energy resolution
+// Then it convolves the resulting flux (in E_nu, CosTheta) with the systematic response array
+// The result is an array of proxy E_mu vs CosTheta which can be used to weigh the monte carlo to get the predicted event rate at icecube
+//
+//
+//  Good things to remember. True/Reco Nu Energy Bins: 200, Cos(Theta) bins: 21, Proxy Reco Mu Energy Bins: 10
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+
 #include <vector>
 #include <iostream>
 #include <ostream>
@@ -13,7 +26,7 @@ using namespace nusquids;
 int fluxprop(int part){
 
   squids::Const units;
-  unsigned int numneu = 3;
+  unsigned int numneu = 3;  // number of neutrinos
   bool interactions = true;
   std::string partsrc;
   if (part == 0)
@@ -22,6 +35,9 @@ int fluxprop(int part){
     partsrc = "kaon";
 
 
+  //
+  // First,  load up the initial flux!
+  //
   std::vector < std::vector < float > > vec_energy, vec_zenith, vec_nuflux, vec_nubarflux;
   vec_energy.resize(150,std::vector<float>(40));
   vec_zenith.resize(150,std::vector<float>(40));
@@ -40,6 +56,9 @@ int fluxprop(int part){
   }
   f.close();
 
+  //
+  // Set up nuSQuiDS
+  //
   //Minimum and maximum values for the energy and cosine zenith, notice that the energy needs to have the
   //units, if they are omitted the input is in eV.
   double Emin=1*units.GeV;
@@ -61,6 +80,8 @@ int fluxprop(int part){
   nus_atm.Set_SquareMassDifference(2,0.00247);
 
   nus_atm.Set_CPPhase(0,2,0);
+
+  // Here is where we'd put in NSI or a 4h neutrino. There are details in the nusquids paper
 
   //Setup integration precision
   nus_atm.Set_rel_error(1.0e-6);
@@ -93,23 +114,18 @@ int fluxprop(int part){
   nus_atm.Set_ProgressBar(false);
   nus_atm.Set_IncludeOscillations(true);
 
-  //Set the initial state in the atmSQuIDS object
-  nus_atm.Set_initial_state(inistate,flavor);
+  // Propagate! Everything!
   nus_atm.EvolveState();
 
-  //This file will contain the final flux, since initially we set it to 1 for the muon,
-  //this can be read as the muon ration F_final/F_initial in cos(zenith) and energy.
-  //std::ofstream file("libflux/flux_"+partsrc+"_dm2_"+std::to_string(im)+"_sin22th_"+std::to_string(is)+".txt");
-  std::ofstream file("libflux/flux_"+partsrc+"_nominal_avg.txt");
 
-  //Set the resolution and the ranges for the ouput, remember that an interpolation in energy is used in
-  //in the interaction picture, the vacuum oscillations are solve analytically with arbitrary Energy precision.
-  //For the zenith a linear interpolation is used.
-
-  // We're gonna average 4 evaluations per bin.
+  //
+  // Get bin averaged, propagated flux at icecube detector
+  //
+  // We're gonna average 4 evaluations per bin. Energy is true Nu Energy.
+  // Number of bins come from resolution of detector, so don't toy too much.
   int nZBins = 21;
   int nEBins = 200;
-  int nAvg = 4;
+  int nAvg = 4;           // How many measurements we average together per bin
 
   // Bin edges
   float binsE[nEBins+1], binsCosZ[nZBins+1];
@@ -121,8 +137,10 @@ int fluxprop(int part){
     binsE[j] = pow(10,log10(200) + j*log10(1e6/200)/nEBins);
   }
 
-  // Now put it all together and write that shit to file
-  file << "# cos(zenith) E flux_nu flux nubar . . . ." << std::endl;
+  std::vector <std::vector < float > > atmFlux_nu, atmFlux_nubar;
+  atmFlux_nu.resize(21,std::vector<float>(200));
+  atmFlux_nubar.resize(21,std::vector<float>(200));
+
   float flux_nu, flux_nubar, centerCZ, centerE;
   for(int cz = 0; cz < nZBins; cz++){
     for(int re = 0; re < nEBins; re++){
@@ -138,12 +156,77 @@ int fluxprop(int part){
       // Get energy and cos(zenith) at center of bin
       centerCZ = binsCosZ[cz] + (binsCosZ[cz+1]-binsCosZ[cz])/2;
       centerE  = pow(10,log10(binsE[re]) + log10(binsE[re+1]/binsE[re])/2);
-      file << " " << centerCZ << " " << centerE;
-      file << " " << flux_nu/(nAvg*nAvg) << " " << flux_nubar/(nAvg*nAvg);
-      file << std::endl;
+      atmFlux_nu[cz][re] = flux_nu/(nAvg*nAvg);
+      atmFlux_nubar[cz][re] = flux_nubar/(nAvg*nAvg);
     }
   }
-  file.close();
+
+  //
+  //  Not done yet. Now, we invoke the systematic response array!
+  //
+  std::vector < std::vector < std::vector <float> > > systematicResponseArrayNu, systematicResponseArrayNubar;
+  systematicResponseArrayNu.resize(10,std::vector  < std::vector <float> >(21,std::vector <float>(200)));
+  systematicResponseArrayNubar.resize(10,std::vector  < std::vector <float> >(21,std::vector <float>(200)));
+
+  f.open("detectorResponseArray.txt");
+  // Skip the first 3 lines
+  for(int i = 0; i < 3; i++)
+    f.ignore(300,'\n');
+  float dummy;
+  std::string sdummy;
+  for(int cz = 0; cz < 21; cz++)  for(int re = 0; re < 200; re++){
+    // first, nu
+    f >> sdummy;
+    f >> dummy;
+    f >> dummy;
+    for(int pe = 0; pe < 10; pe++)
+      f >> systematicResponseArrayNu[pe][cz][re];
+    // nubar!
+    f >> sdummy;
+    f >> dummy;
+    f >> dummy;
+    for(int pe = 0; pe < 10; pe++)
+      f >> systematicResponseArrayNubar[pe][cz][re];
+  }
+  f.close();
+
+  //
+  // Convolve the systematics with the flux
+  //
+  // This is our final flux. It is in terms of proxy reco mu energy and cos zenith angle
+  std::vector < std::vector <float > > flux_conv_nu, flux_conv_nubar;
+  flux_conv_nu.resize(21,std::vector<float>(10));
+  flux_conv_nubar.resize(21,std::vector<float>(10));
+
+
+  float eflux_nu, eflux_nubar;
+  for(int cz = 0; cz < 21; cz++){
+    for(int pe = 0; pe < 10; pe ++){
+      eflux_nu = 0;
+      eflux_nubar = 0;
+      for(int re = 0; re < 200; re ++){
+        eflux_nu += systematicResponseArrayNu[pe][cz][re] * atmFlux_nu[cz][re];
+        eflux_nubar += systematicResponseArrayNubar[pe][cz][re] *atmFlux_nu[cz][re];
+      }
+      flux_conv_nu[cz][pe] = eflux_nu;
+      flux_conv_nubar[cz][pe] = eflux_nubar;
+    }
+  }
+
+  //
+  //  Add the nu and nubar together and spit out a 21 x 10 array in a nice text file. THEN WE'RE DONE!
+  //
+  std::ofstream outfile;
+  outfile.open ("flux_convolved_"+partsrc+".txt");
+  for(int cz = 0; cz < 21; cz++){
+    for(int pe = 0; pe < 10; pe++){
+      outfile << flux_conv_nu[cz][pe] + flux_conv_nubar[cz][pe];
+    }
+  }
+  outfile.close();
+
+// TODO: throw in the corresponding cosZenith and proxy energy for each element so it's at all usable. now it's just a straight list.
+
 
   return 0;
 }
